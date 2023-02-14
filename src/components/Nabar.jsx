5b4f5bbae17138ece6b205f39defa8862e169399
NevermoreBetter <72225613+NevermoreBetter@users.noreply.github.com>
2023-02-14 22:33:57 +0200

    1

--- src/components/Nabar.jsx
+++ src/components/Nabar.jsx
@@ -1,7 +1,13 @@
-import React from "react";
-import { BsFillMoonFill } from "react-icons/bs";
+import React, { useState } from "react";
+import { BsFillMoonFill, BsSunFill } from "react-icons/bs";
 
 function Navbar(props) {
+  const [mode, setMode] = useState(true);
+
+  const toggleMode = () => {
+    setMode(!mode);
+  };
+
   return (
     <div className="flex bg-gray-700 text-white py-9 px-6  justify-between drop-shadow-xl  duration-500 dark:bg-white dark:text-black dark:duration-500 dark:drop-shadow-md">
       <h2 className="font-bold md:text-xl">Where in the world?</h2>
@@ -9,8 +15,10 @@ function Navbar(props) {
         className="flex items-center justify-between gap-2"
         onClick={() => props.handleClick()}
       >
-        <BsFillMoonFill />
-        <button className=" capitalize">Dark mode</button>
+        {mode ? <BsSunFill /> : <BsFillMoonFill />}
+        <button className="capitalize" onClick={() => toggleMode()}>
+          Toggle mode
+        </button>
       </div>
     </div>
   );
